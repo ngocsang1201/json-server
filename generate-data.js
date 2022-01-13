1bfbacc3e759c920a0a5daef67bd7f1f65ef9f87
Sangg <ngocsang1201@gmail.com>
2022-01-13 16:02:57 +0700

    update url

--- generate-data.js
+++ generate-data.js
@@ -1,12 +1,17 @@
 import faker from 'faker';
 import fs from 'fs';
 
-function randomProductList(numOfProducts) {
+const randomNumber = (to, from = 0) =>
+  Math.trunc(Math.random() * (to - from) + from);
+
+const randomProductList = (numOfProducts) => {
   if (numOfProducts <= 0) return [];
 
   const productList = [];
 
   Array.from(new Array(numOfProducts)).forEach((_, index) => {
+    const imageId = randomNumber(1000, 100);
+
     const product = {
       id: faker.datatype.uuid(),
       index: index,
@@ -14,7 +19,7 @@ function randomProductList(numOfProducts) {
       color: faker.commerce.color(),
       price: Number.parseFloat(faker.commerce.price()),
       description: faker.commerce.productDescription(),
-      thumbnailUrl: faker.image.imageUrl(500, 500),
+      thumbnailUrl: `https://picsum.photos/id/${imageId}/1368/400`,
       createdAt: Date.now(),
       updatedAt: Date.now(),
     };
@@ -23,25 +28,25 @@ function randomProductList(numOfProducts) {
   });
 
   return productList;
-}
+};
 
-function randomTags() {
-  const numOfTags = Math.trunc(Math.random() * 4);
+const randomTags = () => {
+  const numOfTags = randomNumber(4);
 
   const tagList = Array.from(new Array(numOfTags)).map(() => {
     return faker.commerce.productAdjective();
   });
 
   return tagList;
-}
+};
 
-function randomPostList(numOfPosts) {
+const randomPostList = (numOfPosts) => {
   if (numOfPosts <= 0) return [];
 
   const postList = [];
 
   Array.from(new Array(numOfPosts)).forEach(() => {
-    const randomNumber = Math.trunc(Math.random() * 9000 + 1000);
+    const imageId = randomNumber(1000, 100);
 
     const post = {
       id: faker.datatype.uuid(),
@@ -50,7 +55,7 @@ function randomPostList(numOfPosts) {
       title: faker.name.title(),
       description: faker.lorem.sentence(20),
       content: faker.lorem.paragraphs(),
-      thumbnail: `https://picsum.photos/id/${randomNumber}/1368/400`,
+      thumbnail: `https://picsum.photos/id/${imageId}/1368/400`,
       tags: randomTags(),
       createdAt: faker.date.past(),
       updatedAt: faker.date.recent(),
@@ -60,7 +65,7 @@ function randomPostList(numOfPosts) {
   });
 
   return postList;
-}
+};
 
 (() => {
   const productList = randomProductList(50);
